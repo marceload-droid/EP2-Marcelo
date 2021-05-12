bac57976915fc67a02348630771593ad8acffddc
marceload-droid <81190008+marceload-droid@users.noreply.github.com>
2021-05-11 22:52:22 -0300

    Funções

---
+++ Funções.py
@@ -0,0 +1,13 @@
+def cria_baralho():
+    valor = ['A', 'K', 'J', 'Q','2','3','4','5','6', '7','8','9', '10']
+    cartas = []
+    for i in valor:
+        a = i+'♥'
+        cartas.append(a)
+        b = i+'♦'
+        cartas.append(b)
+        c = i+'♠'
+        cartas.append(c)
+        d = i+'♣'
+        cartas.append(d)
+    return cartas
